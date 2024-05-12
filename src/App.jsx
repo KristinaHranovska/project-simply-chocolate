be0274f1954c772c6a8ae24708e17e6a3369385a
Kristina <kr.hranovska@gmail.com>
2024-05-12 22:18:42 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import "./shared/styles/variables.scss";
+import "./shared/styles/breakpoints.scss";
+import "./shared/styles/colors.scss";
+import "./shared/styles/mixins.scss";
+import "./shared/styles/base.scss";
+
 const App = () => {
   return (
     <>
