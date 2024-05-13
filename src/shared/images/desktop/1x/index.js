518a94cf4fbd03a26c3a3d56cc327b15a8c88eb4
Kristina <kr.hranovska@gmail.com>
2024-05-13 18:11:08 +0300

    add pictures

---
+++ src/shared/images/desktop/1x/index.js
@@ -0,0 +1,26 @@
+import subscribe from './subscribe.webp'
+import semi from './semi.webp'
+import popup from './popup.webp'
+import milk from './milk.webp'
+import nuts from './nuts.webp'
+import milkChocolate from './milkChocolate.webp'
+import hero from './hero.webp'
+import darkChocolate from './darkChocolate.webp'
+import chocolateWithFruits from './chocolateWithFruits.webp'
+import chocolateWithNuts from './chocolateWithNuts.webp'
+
+
+const imagesDesktop1x = {
+    subscribe,
+    semi,
+    popup,
+    milk,
+    nuts,
+    milkChocolate,
+    hero,
+    darkChocolate,
+    chocolateWithFruits,
+    chocolateWithNuts
+}
+
+export default imagesDesktop1x;
---
+++ src/shared/images/desktop/2x/index.js
@@ -0,0 +1,26 @@
+import subscribe2x from './subscribe2x.webp'
+import semi2x from './semi2x.webp'
+import popup2x from './popup2x.webp'
+import milk2x from './milk2x.webp'
+import nuts2x from './nuts2x.webp'
+import milkChocolate2x from './milkChocolate2x.webp'
+import hero2x from './hero2x.webp'
+import darkChocolate2x from './darkChocolate2x.webp'
+import chocolateWithFruits2x from './chocolateWithFruits2x.webp'
+import chocolateWithNuts2x from './chocolateWithNuts2x.webp'
+
+
+const imagesDesktop2x = {
+    subscribe2x,
+    semi2x,
+    popup2x,
+    milk2x,
+    nuts2x,
+    milkChocolate2x,
+    hero2x,
+    darkChocolate2x,
+    chocolateWithFruits2x,
+    chocolateWithNuts2x
+}
+
+export default imagesDesktop2x;
