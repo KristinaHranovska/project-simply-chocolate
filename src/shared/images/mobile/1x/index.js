bd2eee19a9b0cd648495ab2ef19635723aecbc69
Kristina <kr.hranovska@gmail.com>
2024-05-15 00:03:20 +0300

    mobile is ready

--- src/shared/images/mobile/1x/index.js
+++ src/shared/images/mobile/1x/index.js
@@ -1,24 +1,28 @@
-import subscribe from './subcribe.webp'
-import semi from './semi.webp'
+import big from './big.webp'
+import heroSlideFive from './heroSlideFive.webp'
+import heroSlideFour from './heroSlideFour.webp'
+import heroSlideOne from './heroSlideOne.webp'
+import heroSlideTree from './heroSlideThree.webp'
+import heroSlideTwo from './heroSlideTwo.webp'
+import smallFour from './smallFour.webp'
+import smallOne from './smallOne.webp'
+import smallThree from './smallThree.webp'
+import smallTwo from './smallTwo.webp'
 import popup from './popup.webp'
-import milk from './milk.webp'
-import nuts from './nuts.webp'
-import milkChocolate from './milkChocolate.webp'
-import hero from './hero.webp'
-import chocolateWithFruits from './chocolateWithFruits.webp'
-import chocolateWithNuts from './chocolateWithNuts.webp'
 
 
 const imagesMobile1x = {
-    subscribe,
-    semi,
-    popup,
-    milk,
-    nuts,
-    milkChocolate,
-    hero,
-    chocolateWithFruits,
-    chocolateWithNuts
+    big,
+    heroSlideFive,
+    heroSlideFour,
+    heroSlideOne,
+    heroSlideTree,
+    heroSlideTwo,
+    smallFour,
+    smallOne,
+    smallThree,
+    smallTwo,
+    popup
 }
 
 export default imagesMobile1x;
