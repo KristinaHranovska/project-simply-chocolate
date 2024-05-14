36a8d78deed7e11e714314095b306b123473ec63
Kristina <kr.hranovska@gmail.com>
2024-05-15 00:10:40 +0300

    pictures are ready

--- src/shared/images/tablet/2x/index.js
+++ src/shared/images/tablet/2x/index.js
@@ -1,24 +1,34 @@
-import subscribe2x from './subcribe2z.webp'
-import semi2x from './semi2x.webp'
-import popup2x from './popup2x.webp'
+import big2x from './big2x.webp'
+import chef2x from './chef2x.webp'
 import milk2x from './milk2x.webp'
 import nuts2x from './nuts2x.webp'
 import milkChocolate2x from './milkChocolate2x.webp'
-import hero2x from './hero2x.webp'
-import chocolateWithFruits2x from './chocolateWithFruits2x.webp'
-import chocolateWithNuts2x from './chocolateWithNuts2x.webp'
+import heroSlideFive2x from './heroSlideFive2x.webp'
+import heroSlideFour2x from './heroSlideFour2x.webp'
+import heroSlideOne2x from './heroSlideOne2x.webp'
+import heroSlideTree2x from './heroSlideThree2x.webp'
+import heroSlideTwo2x from './heroSlideTwo2x.webp'
+import smallFour2x from './smallFour2x.webp'
+import smallOne2x from './smallOne2x.webp'
+import smallThree2x from './smallThree2x.webp'
+import smallTwo2x from './smallTwo2x.webp'
 
 
 const imagesTablet2x = {
-    subscribe2x,
-    semi2x,
-    popup2x,
+    big2x,
+    chef2x,
     milk2x,
     nuts2x,
     milkChocolate2x,
-    hero2x,
-    chocolateWithFruits2x,
-    chocolateWithNuts2x
+    heroSlideFive2x,
+    heroSlideFour2x,
+    heroSlideOne2x,
+    heroSlideTree2x,
+    heroSlideTwo2x,
+    smallFour2x,
+    smallOne2x,
+    smallThree2x,
+    smallTwo2x
 }
 
 export default imagesTablet2x;
