0534d1c04fae19ac4c1ccc2d2fd121b71e77bf16
Kristina <kr.hranovska@gmail.com>
2024-05-15 00:06:39 +0300

    tablet is ready

--- src/shared/images/desktop/2x/index.js
+++ src/shared/images/desktop/2x/index.js
@@ -1,6 +1,5 @@
 import big2x from './big2x.webp'
 import chef2x from './chef2x.webp'
-import popupSubscribe2x from './popupSubscribe2x.webp'
 import milk2x from './milk2x.webp'
 import nuts2x from './nuts2x.webp'
 import milkChocolate2x from './milkChocolate2x.webp'
@@ -9,20 +8,15 @@ import heroSlideFour2x from './heroSlideFour2x.webp'
 import heroSlideOne2x from './heroSlideOne2x.webp'
 import heroSlideTree2x from './heroSlideThree2x.webp'
 import heroSlideTwo2x from './heroSlideTwo2x.webp'
-import line2x from './line2x.webp'
-import lineDown2x from './lineDown2x.webp'
-import lineUp2x from './lineUp2x.webp'
-import shef2x from './shef2x.webp'
 import smallFour2x from './smallFour2x.webp'
 import smallOne2x from './smallOne2x.webp'
 import smallThree2x from './smallThree2x.webp'
 import smallTwo2x from './smallTwo2x.webp'
 
 
-const imagesDesktop2x = {
+const imagesTablet2x = {
     big2x,
     chef2x,
-    popupSubscribe2x,
     milk2x,
     nuts2x,
     milkChocolate2x,
@@ -31,14 +25,10 @@ const imagesDesktop2x = {
     heroSlideOne2x,
     heroSlideTree2x,
     heroSlideTwo2x,
-    line2x,
-    lineDown2x,
-    lineUp2x,
-    shef2x,
     smallFour2x,
     smallOne2x,
     smallThree2x,
     smallTwo2x
 }
 
-export default imagesDesktop2x;
+export default imagesTablet2x;
