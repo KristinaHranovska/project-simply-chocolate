1c035a9b072bfccb27f47de894f4ddc1d4e33b31
Kristina <kr.hranovska@gmail.com>
2024-05-15 15:28:39 +0300

    Update Header.jsx

--- src/modules/header/components/Header/Header.jsx
+++ src/modules/header/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { BasketButton } from 'shared/components';
+import { BasketButton } from '@shared/components';
 import HeaderNav from '../HeaderNav/HeaderNav';
 import Logo from '../Logo/Logo';
 
